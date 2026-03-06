19181f2c70d351a3081d01079e1137b60ab2e329
Arun Menezes <2026leapoffaith@gmail.com>
2026-03-06 08:49:52 -0800

    Add Netlify appointment function

---
+++ netlify/functions/appointment.js
@@ -0,0 +1,24 @@
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+    };
+  }
+
+  let data = {};
+  try {
+    data = JSON.parse(event.body || "{}");
+  } catch {
+    return {
+      statusCode: 400,
+      body: "Invalid JSON",
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ok: true, received: data }),
+  };
+};
